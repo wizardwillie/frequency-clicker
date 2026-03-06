91eccea4d988e59cc70ca2ed4035e638d4977c71
wizardwillie <108853423+wizardwillie@users.noreply.github.com>
2026-03-05 19:24:27 -0500

    Update game.js

--- src/game.js
+++ src/game.js
@@ -1,1 +1,40 @@
+export class Game {
 
+    constructor(canvas) {
+
+        this.canvas = canvas
+        this.ctx = canvas.getContext("2d")
+
+        this.lastTime = 0
+
+    }
+
+    start() {
+
+        requestAnimationFrame(this.loop.bind(this))
+
+    }
+
+    loop(time) {
+
+        const delta = (time - this.lastTime) / 1000
+        this.lastTime = time
+
+        this.update(delta)
+        this.render()
+
+        requestAnimationFrame(this.loop.bind(this))
+
+    }
+
+    update(delta) {
+
+    }
+
+    render() {
+
+        this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height)
+
+    }
+
+}
